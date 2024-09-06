4b5face38b687fdab1f14db0feb686873a1177dd
jay-taylerson <87064668+jay-taylerson@users.noreply.github.com>
2024-09-06 11:39:25 +0100

    Sync to genesiscommunity

---
+++ server/howto-ui-integrations-angular-app/build.gradle.kts
@@ -0,0 +1,18 @@
+dependencies {
+    implementation("global.genesis:file-server-app:${project.ext["fileServerVersion"]}")
+    compileOnly(genesis("script-dependencies"))
+    genesisGeneratedCode(withTestDependency = true)
+    testImplementation(genesis("dbtest"))
+    testImplementation(genesis("testsupport"))
+    testImplementation(genesis("pal-eventhandler"))
+}
+
+description = "OrderMgmt-app"
+
+sourceSets {
+    main {
+        resources {
+            srcDirs("src/main/resources", "src/main/genesis")
+        }
+    }
+}
