806caf1e0b5d10c1496523da84d612b0dc9f61e8
jay-taylerson <87064668+jay-taylerson@users.noreply.github.com>
2024-09-06 11:39:25 +0100

    Sync to genesiscommunity

---
+++ client/src/app/pages/trade-mgmt/trade-mgmt-manager/trade-mgmt.update.form.schema.ts
@@ -0,0 +1,37 @@
+import { UiSchema } from '@genesislcap/foundation-forms';
+
+export const updateFormSchema: UiSchema = {
+  "type": "VerticalLayout",
+  "elements": [
+    {
+      "type": "Control",
+      "label": "Trade Id",
+      "scope": "#/properties/TRADE_ID",
+      "options": {
+        "readonly": true
+      }
+    },
+    {
+      "type": "Control",
+      "label": "Traded Price",
+      "scope": "#/properties/TRADED_PRICE",
+      "options": {}
+    },
+    {
+      "type": "Control",
+      "label": "Traded Quantity",
+      "scope": "#/properties/TRADED_QUANTITY",
+      "options": {}
+    },
+    {
+      "type": "Control",
+      "label": "Order Id",
+      "scope": "#/properties/ORDER_ID",
+      "options": {
+        "allOptionsResourceName": "ORDER",
+        "valueField": "ORDER_ID",
+        "labelField": "ORDER_ID"
+      }
+    }
+  ]
+}
